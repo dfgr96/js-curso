8b5f662e6af6000f0ead10dda884189443dd3f14
Felipe Gomez Rodriguez <davidfelgomezr@hotmail.com>
2022-10-03 22:03:34 -0500

    Feat: create an update/delete list with js and html

---
+++ main.js
@@ -0,0 +1,46 @@
+const toDos = JSON.parse(localStorage.getItem('todosLS')) || [];
+
+const render = () => {
+    const toDoList = document.getElementById('toDo-list');
+
+    const toDosTemplate = toDos.map(t => '<li>' + t + '</li>');
+
+    toDoList.innerHTML = toDosTemplate.join('');
+
+    const elements = document.querySelectorAll('#toDo-list li')
+    elements.forEach((element, i) => {
+
+        element.addEventListener('click', () => {
+            console.log(element, i)
+            element.parentNode.removeChild(element);
+            toDos.splice(i, 1)
+            updateToDos(toDos);
+            render();
+
+        })
+    })
+}
+
+window.onload = () => {
+    render();
+    const form = document.getElementById('toDo-form');
+    form.onsubmit = (e) => {
+        e.preventDefault();
+
+        const toDo = document.getElementById('toDo');
+        const toDoText = toDo.value;
+        toDo.value = '';
+
+        toDos.push(toDoText);
+        updateToDos(toDos);
+        render();
+
+        console.log(toDos)
+
+    }
+}
+
+const updateToDos = (toDos) => {
+    const toDosString = JSON.stringify(toDos);
+    localStorage.setItem('todosLS', toDosString);
+}
